b03db5327ba72f1e94d71321c29ea3a6c51c2fb0
maker <makar-b01@mail.ru>
2022-09-23 18:21:39 +0200

    this code is BAD

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -1,18 +1,19 @@
-'''
+"""
 Eight Puzzle Problem
 
-'''
+"""
 
 from copy import deepcopy
 
-class EightPuzzle:
 
-    def __init__(self, initial_state, goal_state):
+class EightPuzzle:
+    def __init__(self, initial_state, goal_state, verbose = False):
         self.state = initial_state
         self.goal = goal_state
-        self.action = ['ml','mr','mu','md']
+        self.action = ["ml", "mr", "mu", "md"]
+        self.verbose = verbose
         global h2
-            
+
     def check_goal(self):
         if self.state == self.goal:
             return True
@@ -26,89 +27,95 @@ class EightPuzzle:
         global h1
         h1 = 0
         state = self.state.state
-        for i in range(0,len(state)):
-            for z in range(0,len(state)):
+        for i in range(0, len(state)):
+            for z in range(0, len(state)):
                 if state[i][z] == self.state.goal[i][z]:
                     h1 = h1 + 1
-        h1 = len(state)**2 - h1
-        print ('Heuristic_1: ' + str(h1))
+        # h1 = len(state) ** 2 - h1
+        if self.verbose:
+            print("Heuristic_1: " + str(h1))
         return h1
 
     def heuristic2(self):
-        state = self.state
-        goal = self.goal
-        h2=self.h2
+        global h2
+        h2 = 0
+        state = self.state.state
+        goal = [["e", 1, 2], [3, 4, 5], [6, 7, 8]]
         x_goal = 0
         y_goal = 0
         number = 0
         x_value = 0
         y_value = 0
-        for i in range(0,len(state)):
-            for z in range(0,len(state)):
-                if state[i][z] != goal[i][z]:                        
+        for i in range(0, len(state)):
+            for z in range(0, len(state)):
+                if state[i][z] != goal[i][z]:
                     number = state[i][z]
-                    x_value=i
-                    y_value=z
-                    for x in range(0,len(state)):
-                        for y in range(0,len(state)):
-                            if number == 'e':
-                                number == 'Null'
-                                x_goal=x_value
-                                y_goal=x_value
+                    x_value = i
+                    y_value = z
+                    for x in range(0, len(state)):
+                        for y in range(0, len(state)):
+                            if number == "e":
+                                number == "Null"
+                                x_goal = x_value
+                                y_goal = x_value
                                 break
                             elif goal[x][y] == number:
-                                x_goal=x
-                                y_goal=y
+                                x_goal = x
+                                y_goal = y
                 h2 = h2 + abs(x_value - x_goal) + abs(y_value - y_goal)
-        print ('Heuristic_2: ' +str(h2))
+        if self.verbose:
+            print("Heuristic_2: " + str(h2))
         return h2
 
-
     def move(self, move):
-        if move == 'ml': #move left
+        if move == "ml":  # move left
             dc = deepcopy(self)
             if dc.ml():
                 return dc
-        elif move == 'mr': #move right
+        elif move == "mr":  # move right
             dc = deepcopy(self)
             if dc.mr():
                 return dc
-        if move == 'mu': #move up
+        if move == "mu":  # move up
             dc = deepcopy(self)
             if dc.mu():
                 return dc
-        elif move == 'md': #move down
+        elif move == "md":  # move down
             dc = deepcopy(self)
             if dc.md():
                 return dc
 
     def ml(self):
 
-        if (self.state[0][0] != 'e' and self.state[1][0] != 'e' and self.state[2][0] != 'e'):
-            if ('e' in self.state[0]):
-                if self.state[0][1] == 'e':
+        if (
+            self.state[0][0] != "e"
+            and self.state[1][0] != "e"
+            and self.state[2][0] != "e"
+        ):
+            if "e" in self.state[0]:
+                if self.state[0][1] == "e":
                     temp_val = self.state[0][0]
                     self.state[0][0] = self.state[0][1]
                     self.state[0][1] = temp_val
-                elif self.state[0][2] == 'e':
+                elif self.state[0][2] == "e":
                     temp_val = self.state[0][1]
                     self.state[0][1] = self.state[0][2]
                     self.state[0][2] = temp_val
-            elif ('e' in self.state[1]):
-                if self.state[1][1] == 'e':
+            elif "e" in self.state[1]:
+                if self.state[1][1] == "e":
                     temp_val = self.state[1][0]
                     self.state[1][0] = self.state[1][1]
                     self.state[1][1] = temp_val
-                elif self.state[1][2] == 'e':
+                elif self.state[1][2] == "e":
                     temp_val = self.state[1][1]
                     self.state[1][1] = self.state[1][2]
                     self.state[1][2] = temp_val
             else:
-                if self.state[2][1] == 'e':
+                if self.state[2][1] == "e":
                     temp_val = self.state[2][0]
                     self.state[2][0] = self.state[2][1]
                     self.state[2][1] = temp_val
-                elif self.state[2][2] == 'e':
+                elif self.state[2][2] == "e":
                     temp_val = self.state[2][1]
                     self.state[2][1] = self.state[2][2]
                     self.state[2][2] = temp_val
@@ -116,31 +123,35 @@ class EightPuzzle:
 
     def mr(self):
 
-        if (self.state[0][2] != 'e' and self.state[1][2] != 'e' and self.state[2][2] != 'e'):
-            if ('e' in self.state[0]):
-                if self.state[0][0] == 'e':
+        if (
+            self.state[0][2] != "e"
+            and self.state[1][2] != "e"
+            and self.state[2][2] != "e"
+        ):
+            if "e" in self.state[0]:
+                if self.state[0][0] == "e":
                     temp_val = self.state[0][1]
                     self.state[0][1] = self.state[0][0]
                     self.state[0][0] = temp_val
-                elif self.state[0][1] == 'e':
+                elif self.state[0][1] == "e":
                     temp_val = self.state[0][2]
                     self.state[0][2] = self.state[0][1]
                     self.state[0][1] = temp_val
-            elif ('e' in self.state[1]):
-                if self.state[1][0] == 'e':
+            elif "e" in self.state[1]:
+                if self.state[1][0] == "e":
                     temp_val = self.state[1][1]
                     self.state[1][1] = self.state[1][0]
                     self.state[1][0] = temp_val
-                elif self.state[1][1] == 'e':
+                elif self.state[1][1] == "e":
                     temp_val = self.state[1][2]
                     self.state[1][2] = self.state[1][1]
                     self.state[1][1] = temp_val
             else:
-                if self.state[2][0] == 'e':
+                if self.state[2][0] == "e":
                     temp_val = self.state[2][1]
                     self.state[2][1] = self.state[2][0]
                     self.state[2][0] = temp_val
-                elif self.state[2][1] == 'e':
+                elif self.state[2][1] == "e":
                     temp_val = self.state[2][2]
                     self.state[2][2] = self.state[2][1]
                     self.state[2][1] = temp_val
@@ -148,30 +159,30 @@ class EightPuzzle:
 
     def mu(self):
 
-        if ('e' not in self.state[0]):
-            if ('e' in self.state[1]):
-                if self.state[1][0] == 'e':
+        if "e" not in self.state[0]:
+            if "e" in self.state[1]:
+                if self.state[1][0] == "e":
                     temp_val = self.state[0][0]
                     self.state[0][0] = self.state[1][0]
                     self.state[1][0] = temp_val
-                elif self.state[1][1] == 'e':
+                elif self.state[1][1] == "e":
                     temp_val = self.state[0][1]
                     self.state[0][1] = self.state[1][1]
                     self.state[1][1] = temp_val
-                elif self.state[1][2] == 'e':
+                elif self.state[1][2] == "e":
                     temp_val = self.state[0][2]
                     self.state[0][2] = self.state[1][2]
                     self.state[1][2] = temp_val
-            elif ('e' in self.state[2]):
-                if self.state[2][0] == 'e':
+            elif "e" in self.state[2]:
+                if self.state[2][0] == "e":
                     temp_val = self.state[1][0]
                     self.state[1][0] = self.state[2][0]
                     self.state[2][0] = temp_val
-                elif self.state[2][1] == 'e':
+                elif self.state[2][1] == "e":
                     temp_val = self.state[1][1]
                     self.state[1][1] = self.state[2][1]
                     self.state[2][1] = temp_val
-                elif self.state[2][2] == 'e':
+                elif self.state[2][2] == "e":
                     temp_val = self.state[1][2]
                     self.state[1][2] = self.state[2][2]
                     self.state[2][2] = temp_val
@@ -179,40 +190,38 @@ class EightPuzzle:
 
     def md(self):
 
-        if ('e' not in self.state[2]):
-            if ('e' in self.state[0]):
-                if self.state[0][0] == 'e':
+        if "e" not in self.state[2]:
+            if "e" in self.state[0]:
+                if self.state[0][0] == "e":
                     temp_val = self.state[1][0]
                     self.state[1][0] = self.state[0][0]
                     self.state[0][0] = temp_val
-                elif self.state[0][1] == 'e':
+                elif self.state[0][1] == "e":
                     temp_val = self.state[1][1]
                     self.state[1][1] = self.state[0][1]
                     self.state[0][1] = temp_val
-                elif self.state[0][2] == 'e':
+                elif self.state[0][2] == "e":
                     temp_val = self.state[1][2]
                     self.state[1][2] = self.state[0][2]
                     self.state[0][2] = temp_val
-            elif ('e' in self.state[1]):
-                if self.state[1][0] == 'e':
+            elif "e" in self.state[1]:
+                if self.state[1][0] == "e":
                     temp_val = self.state[2][0]
                     self.state[2][0] = self.state[1][0]
                     self.state[1][0] = temp_val
-                elif self.state[1][1] == 'e':
+                elif self.state[1][1] == "e":
                     temp_val = self.state[1][1]
                     self.state[1][1] = self.state[2][1]
                     self.state[2][1] = temp_val
-                elif self.state[1][2] == 'e':
+                elif self.state[1][2] == "e":
                     temp_val = self.state[1][2]
                     self.state[1][2] = self.state[2][2]
                     self.state[2][2] = temp_val
             return True
 
-
     def pretty_print(self):
-        print('----------------------------')
+        print("----------------------------")
         print(self.state[0])
         print(self.state[1])
         print(self.state[2])
-        print('----------------------------')
-
+        print("----------------------------")
--- run_assignment_1.py
+++ run_assignment_1.py
@@ -1,34 +1,39 @@
-'''
+"""
 Define problem and start execution of search problems
 Author: Tony Lindgren
-'''
+"""
 
 
 from tabnanny import verbose
 from eight_puzzle import EightPuzzle
-from missionaries_and_cannibals import MissionariesAndCannibals 
+from missionaries_and_cannibals import MissionariesAndCannibals
 from node_and_search import SearchAlgorithm
 from node_and_search import Node
 
-#init_state = [[0, 0], 'r', [3, 3]] 
-#goal_state = [[3, 3], 'l', [0, 0]] 
+# init_state = [[0, 0], 'r', [3, 3]]
+# goal_state = [[3, 3], 'l', [0, 0]]
 
-init_state = [[7,2,4],[5,'e',6],[1,3,8]]
-goal_state = [['e',1,2],[3,4,5],[6,7,8]]
+init_state = [[1, 2, 3], [4, 5, 6], [7, 8, "e"]]
+goal_state = [["e", 1, 2], [3, 4, 5], [6, 7, 8]]
+
 
 def main():
-    #mc = MissionariesAndCannibals(init_state, goal_state)
+    # mc = MissionariesAndCannibals(init_state, goal_state)
     # sa = SearchAlgorithm(mc)
-    #solution = sa.bfs(statistics=True, FileSave=True)
-    #solution = sa.dfs(statistics=True, FileSave=True)
-    #solution = sa.ids(statistics=True, FileSave=True)
-    #solution.pretty_print_solution(verbose=True)
-    #print('Confirm solution is: ', solution.state.check_goal())
-    
+    # solution = sa.bfs(statistics=True, FileSave=True)
+    # solution = sa.dfs(statistics=True, FileSave=True)
+    # solution = sa.ids(statistics=True, FileSave=True)
+    # solution.pretty_print_solution(verbose=True)
+    # print('Confirm solution is: ', solution.state.check_goal())
+
     ep = EightPuzzle(init_state, goal_state)
     sa = SearchAlgorithm(ep)
-    solution = sa.greedy_search(statistics=True, FileSave=False)
-    solution.state.pretty_print   
+    solution = sa.greedy_search(depth_limit = 10, verbose=True, statistics=False, FileSave=False)
+    if solution:
+        solution.state.pretty_print()
+    else:
+        print("Pizdec")
+
 
 if __name__ == "__main__":
     main()
